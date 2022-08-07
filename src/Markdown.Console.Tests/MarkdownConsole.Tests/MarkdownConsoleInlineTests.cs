d6970864c9bd8e99f3ec2650e95a639cd9d40c99
David Rushton <david-rushton@outlook.com>
2022-08-07 12:26:28 +0100

    Renames test folder
    Better reflects the item under test

--- src/Markdown.Console.Tests/MarkdownConsole.Tests/MarkdownConsoleInlineTests.cs
+++ src/Markdown.Console.Tests/MarkdownConsole.Tests/MarkdownConsoleInlineTests.cs
@@ -5,16 +5,6 @@ namespace Markdown.Console.Tests;
 
 public partial class MarkdownConsoleTests
 {
-    // https://en.wikipedia.org/wiki/ANSI_escape_code
-    private const string AnsiEscape = "\u001b";
-    private const string AnsiResetEscape = "\u001b[0m";
-    private const string AnsiBoldEscape = "\u001b[1m";
-    private const string AnsiItalicEscape = "\u001b[3m";
-    private const string AnsiStrikethroughEscape = "\u001b[9m";
-    private const string AnsiCodeStartEscape = "\u001b[38;5;5m\u001b[0m\u001b[7;38;5;5m";
-    private const string AnsiCodeEndEscape = "\u001b[0m\u001b[38;5;5m";
-
-
     [Theory]
     [InlineData("**bold**",             $"{AnsiBoldEscape}bold{AnsiResetEscape}\n\n")]
     [InlineData("*bold*",               $"{AnsiBoldEscape}bold{AnsiResetEscape}\n\n")]
