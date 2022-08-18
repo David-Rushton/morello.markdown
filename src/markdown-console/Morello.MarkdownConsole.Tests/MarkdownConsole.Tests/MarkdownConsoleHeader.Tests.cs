dbca8a13606039b0f38a09a593fe03d9d56a796c
David Rushton <david-rushton@outlook.com>
2022-08-19 00:15:46 +0100

    Removes duplicate test

--- src/markdown-console/Morello.MarkdownConsole.Tests/MarkdownConsole.Tests/MarkdownConsoleHeader.Tests.cs
+++ src/markdown-console/Morello.MarkdownConsole.Tests/MarkdownConsole.Tests/MarkdownConsoleHeader.Tests.cs
@@ -41,25 +41,6 @@ public partial class MarkdownConsoleHeaderTests : MarkdownConsoleTests
         Assert.Equal(expected, actual);
     }
 
-    [Theory]
-    [InlineData("## Header", $"{AnsiEscape}[1;38;5;5mHeader{AnsiResetEscape}\n\n\n")]
-    [InlineData("### Header", $"{AnsiEscape}[1;38;5;5mHeader{AnsiResetEscape}\n\n\n")]
-    [InlineData("#### Header", $"{AnsiEscape}[1;38;5;5mHeader{AnsiResetEscape}\n\n\n")]
-    [InlineData("##### Header", $"{AnsiEscape}[1;38;5;5mHeader{AnsiResetEscape}\n\n\n")]
-    [InlineData("###### Header", $"{AnsiEscape}[1;38;5;5mHeader{AnsiResetEscape}\n\n\n")]
-    public void Given_markdown_with_block_header_level_2_to_6_should_return_correct_ansi_escaped_string(
-        string markdown,
-        string expected
-    )
-    {
-        var actual = new TestConsole()
-            .Write(markdown)
-            .Output
-            .NormaliseNewLines();
-
-        Assert.Equal(expected, actual);
-    }
-
     [Fact]
     public void Given_markdown_with_invalid_header_level_block_should_return_correct_ansi_escaped_string()
     {
